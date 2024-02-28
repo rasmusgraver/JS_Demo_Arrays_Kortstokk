65d58e5734e0a09ffa2225b898c3a33d9805afc1
Rasmus Graver <rasmus.graver@gmail.com>
2024-02-28 12:12:45 +0100

    Rydded litt i koden og la inn kommentarer

--- 1_arrays.js
+++ 1_arrays.js
@@ -2,15 +2,29 @@
 let currentSection = null
 
 let cards = ["1_of_clubs", "3_of_hearts", "12_of_spades"]
+// Legger til kort på starten av array:
 cards.unshift("7_of_diamonds")
 cards.unshift("2_of_clubs")
-cards.unshift("1_of_spades")
-cards.unshift("13_of_diamonds")
+// Legger til kort på slutten av array:
+cards.push("1_of_spades")
+cards.push("13_of_diamonds")
+cards.push("5_of_hearts")
 
+// Viser kortene i en blå section:
 addSection("blue")
 addCards(cards)
 
-function sammenliknKort(kortA, kortB) {
+// Sorter kortene, og legg til i rosa section:
+cards.sort(sammenliknKortEtterTall)
+addSection("pink")
+addCards(cards)
+
+
+
+/*
+ * Sorterings-funksjoner:
+ */
+function sammenliknKortEtterTall(kortA, kortB) {
     tallA = parseInt(kortA) // Henter ut tallverdien av kort a
     tallB = parseInt(kortB) // Henter ut tallverdien av kort b
     // Vil at Ess teller som høyeste kort:
@@ -24,11 +38,9 @@ function sammenliknKort(kortA, kortB) {
 }
 
 
-cards.sort(sammenliknKort)
-
-addSection("pink")
-addCards(cards)
-
+/*
+ * Funkskjoner med å legge til sections og cards:
+ */
 function addSection(borderColor) {
     const section = document.createElement("section")
     section.style.borderColor = borderColor
@@ -47,5 +59,3 @@ function addCards(cards) {
     }
 
 }
-
-
